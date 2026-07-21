1fa968fc7612d558e43c075bda1db09e2d09ba8b
Luis Francisco <183943582+luisfrancisco2b@users.noreply.github.com.>
2026-07-21 12:48:43 -0300

    feat(js): cleaning code

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// Selecting the DOM elements
 const container = document.querySelector(".container");
 const qrCodeBtn = document.querySelector("#qr-form button");
 
@@ -6,16 +7,22 @@ const qrCodeInput = document.querySelector("#qr-form input");
 const qrCodeImg = document.querySelector("#qr-code img");
 
 // Function
-// Gerar QR Code
+// Generate QR Code
 function generateQrCode() {
+
+  // Get the text or link the user typed
   const qrCodeInputValue = qrCodeInput.value;
 
+  // Check: if there is no input value, function execution is cancelled
   if (!qrCodeInputValue) return;
 
+  // Visual feedback
   qrCodeBtn.innerText = "Gerando QrCode...";
 
+  // Change the src property of the HTML image, concatenating the API link with the value entered by the user
   qrCodeImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrCodeInputValue}`;
 
+  // Triggers the "load" event for the 'img' tag, adds the ".active" class and changes the button text 
   qrCodeImg.addEventListener("load", () => {
     container.classList.add("active");
 
@@ -24,17 +31,19 @@ function generateQrCode() {
 }
 
 // Events
+// Click event to call the QR Code generation function
 qrCodeBtn.addEventListener("click", () => {
   generateQrCode();
 });
 
+// Triggers the function when the user presses the Enter key
 qrCodeInput.addEventListener("keydown", (event) => {
   if (event.code === "Enter") {
     generateQrCode();
   }
 });
 
-// Limpar área do QR Code
+// Dynamic cleaning of the QR Code area
 qrCodeInput.addEventListener("keyup", () => {
   if (!qrCodeInput.value) {
     container.classList.remove("active");
